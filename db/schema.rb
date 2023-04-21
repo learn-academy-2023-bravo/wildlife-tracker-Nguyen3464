e9e6422bf1bbb2297d02b057cde0190b1c5651a0
Nguyen3464 <n.nguyen3464@gmail.com>
2023-04-21 09:35:10 -0700

    sightings-crud-actions submission

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_220058) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_225436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "animal_sightings", force: :cascade do |t|
+    t.string "latitude"
+    t.string "longitude"
+    t.string "date"
+    t.integer "animal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "animals", force: :cascade do |t|
     t.string "commonnames"
